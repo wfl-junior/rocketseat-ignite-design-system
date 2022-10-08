4b4eed25b8754d5424d31357d476d5b1e6a0ed2b
Wallace Júnior <wflj1997@hotmail.com>
2022-10-08 20:41:40 -0300

    Documentação de cores

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,24 @@
+import { colors } from "@ignite-ui/tokens";
+import { getContrast } from "polished";
+import { Fragment } from "react";
+
+export const ColorsGrid: React.FC = () => (
+  <Fragment>
+    {Object.entries(colors).map(([key, color]) => (
+      <div key={key} style={{ backgroundColor: color, padding: "2rem" }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            fontFamily: "monospace",
+            color: getContrast(color, "white") < 3.5 ? "#111" : "white",
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    ))}
+  </Fragment>
+);
